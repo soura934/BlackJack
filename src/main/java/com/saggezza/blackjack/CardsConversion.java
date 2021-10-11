eb87b71cb2e9da79753911e16b3f4b392df8f408
Mansour Dione <soura934@gmail.com>
2021-10-11 14:44:08 -0400

    write cards conversion

--- src/main/java/com/saggezza/blackjack/CardsConversion.java
+++ src/main/java/com/saggezza/blackjack/CardsConversion.java
@@ -4,7 +4,7 @@ public class CardsConversion implements ICardsConversion{
 
     public String numberToCard(int num) {
         String[] cards = {"A", "2", "3", "4", "5", "6", "7", "8",
-                            "9", "10", "J", "K", "Q"};
+                            "9", "10", "J", "Q", "K"};
         return cards[num -1];
     }
 }
--- src/test/java/com/saggezza/blackjack/CardsConversonTest.java
+++ src/test/java/com/saggezza/blackjack/CardsConversonTest.java
@@ -18,4 +18,18 @@ public class CardsConversonTest  {
 //      Then: I should get back an Ace
         assertEquals("A", result);
     }
+
+    @Test
+    public void numberToCardTest2() {
+
+//      Given: I have the number 1
+        int num = 11 ;
+
+//      When: I convert it to a card
+        ICardsConversion cardsConversion = new CardsConversion();
+        String result = cardsConversion.numberToCard(num);
+
+//      Then: I should get back an Ace
+        assertEquals("J", result);
+    }
 }
