80615ae76c0ccc37aab818d401c35705529336c6
ramshaessa <ressa@luc.edu>
2021-10-12 11:29:40 -0500

    new changes

--- src/main/java/com/saggezza/blackjack/PlayerFlow.java
+++ src/main/java/com/saggezza/blackjack/PlayerFlow.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 public class PlayerFlow implements IPlayerFlow {
 
     private IDrawCard drawCard;
+    private IDisplayFlow displayFlow;
 
-    public PlayerFlow(IDrawCard drawCard) {
+    public PlayerFlow(IDrawCard drawCard, IDisplayFlow displayFlow) {
         this.drawCard = drawCard;
+        this.displayFlow = displayFlow;
     }
 
     public void playerTurn(List<String> cards, List<String> deck) {
@@ -18,10 +20,13 @@ public class PlayerFlow implements IPlayerFlow {
         String userInput = scan.nextLine();
 
         while(userInput.equals("D")) { //while they are drawing cards
-            drawCard.draw(deck); //they would only draw card if they enter D.
+            String newCard = drawCard.draw(deck); //they would only draw card if they enter D.
+            cards.add(newCard);
+            displayFlow.displayCards("player", cards, false);
 
             System.out.println("Enter H to hold or D to Draw");
             userInput = scan.nextLine();
+
         }
 
     }
--- src/test/java/com/saggezza/blackjack/PlayerFlowTest.java
+++ src/test/java/com/saggezza/blackjack/PlayerFlowTest.java
@@ -26,8 +26,10 @@ public class PlayerFlowTest {
         IDrawCard drawCard = mock(IDrawCard.class);
         when(drawCard.draw(deck)).thenReturn("Ace of Spades");
 
+        IDisplayFlow displayFlow = mock(IDisplayFlow.class);
+
         //When: Player decides to draw.
-        IPlayerFlow playerFlow = new PlayerFlow(drawCard);
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow);
         playerFlow.playerTurn(cards,deck);
 
         //Then: Draw card is called once.
@@ -49,13 +51,40 @@ public class PlayerFlowTest {
         IDrawCard drawCard = mock(IDrawCard.class);
         when(drawCard.draw(deck)).thenReturn("Ace of Spades");
 
+        IDisplayFlow displayFlow = mock(IDisplayFlow.class);
+
         //When: Player stands.
-        IPlayerFlow playerFlow = new PlayerFlow(drawCard);
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow);
         playerFlow.playerTurn(cards,deck);
 
         //Then: Draw card is not called.
         verify(drawCard, times(0)).draw(deck);
 
+    }
+
+    @Test
+    public void displayCard() {
+        String userInput = "D\nH";
+        System.setIn(new ByteArrayInputStream(userInput.getBytes())); //Filling in with fake user input
+        List<String> deck = new ArrayList<String>();
+        List<String> cards = new ArrayList<>();
+
+        //Given: Player has a 5,10.
+        cards.add("5 of Hearts");
+        cards.add("10 of Clubs");
+
+        IDrawCard drawCard = mock(IDrawCard.class);
+        when(drawCard.draw(deck)).thenReturn("Ace of Spades");
+
+        IDisplayFlow displayFlow = mock(IDisplayFlow.class);
+
+        //When: Player draws.
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow);
+        playerFlow.playerTurn(cards,deck);
+
+
+        //Then: Cards are displayed.
+        verify(displayFlow, times(1)).displayCards("player", cards, false);
     }
 
 
