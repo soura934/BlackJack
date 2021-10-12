a29dd3e1ae530f8f71917e2385b280323850382b
ramshaessa <ressa@luc.edu>
2021-10-12 12:34:33 -0500

    new changes

--- src/main/java/com/saggezza/blackjack/PlayerFlow.java
+++ src/main/java/com/saggezza/blackjack/PlayerFlow.java
@@ -7,10 +7,12 @@ public class PlayerFlow implements IPlayerFlow {
 
     private IDrawCard drawCard;
     private IDisplayFlow displayFlow;
+    private ICardValues cardValues;
 
-    public PlayerFlow(IDrawCard drawCard, IDisplayFlow displayFlow) {
+    public PlayerFlow(IDrawCard drawCard, IDisplayFlow displayFlow, ICardValues cardValues) {
         this.drawCard = drawCard;
         this.displayFlow = displayFlow;
+        this.cardValues = cardValues;
     }
 
     public void playerTurn(List<String> cards, List<String> deck) {
@@ -23,6 +25,7 @@ public class PlayerFlow implements IPlayerFlow {
             String newCard = drawCard.draw(deck); //they would only draw card if they enter D.
             cards.add(newCard);
             displayFlow.displayCards("player", cards, false);
+           List <Integer> cardVals  = cardValues.getCardValues(cards);
 
             System.out.println("Enter H to hold or D to Draw");
             userInput = scan.nextLine();
--- src/test/java/com/saggezza/blackjack/PlayerFlowTest.java
+++ src/test/java/com/saggezza/blackjack/PlayerFlowTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 
 
-
 public class PlayerFlowTest {
 
     @Test
@@ -18,19 +17,26 @@ public class PlayerFlowTest {
         System.setIn(new ByteArrayInputStream(userInput.getBytes()));
         List<String> deck = new ArrayList<String>();
         List<String> cards = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+
 
         //Given: Player has a 5,10.
         cards.add("5 of Hearts");
         cards.add("10 of Clubs");
+        values.add(5);
+        values.add(10);
 
         IDrawCard drawCard = mock(IDrawCard.class);
         when(drawCard.draw(deck)).thenReturn("Ace of Spades");
 
+        ICardValues cardValues = mock(ICardValues.class);
+        when(cardValues.getCardValues(cards)).thenReturn(values);
+
         IDisplayFlow displayFlow = mock(IDisplayFlow.class);
 
         //When: Player decides to draw.
-        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow);
-        playerFlow.playerTurn(cards,deck);
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues);
+        playerFlow.playerTurn(cards, deck);
 
         //Then: Draw card is called once.
         verify(drawCard, times(1)).draw(deck);
@@ -43,19 +49,26 @@ public class PlayerFlowTest {
         System.setIn(new ByteArrayInputStream(userInput.getBytes())); //Filling in with fake user input
         List<String> deck = new ArrayList<String>();
         List<String> cards = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+
 
         //Given: Player has a 5,10.
         cards.add("5 of Hearts");
         cards.add("10 of Clubs");
+        values.add(5);
+        values.add(10);
 
         IDrawCard drawCard = mock(IDrawCard.class);
         when(drawCard.draw(deck)).thenReturn("Ace of Spades");
 
+        ICardValues cardValues = mock(ICardValues.class);
+        when(cardValues.getCardValues(cards)).thenReturn(values);
+
         IDisplayFlow displayFlow = mock(IDisplayFlow.class);
 
         //When: Player stands.
-        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow);
-        playerFlow.playerTurn(cards,deck);
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues);
+        playerFlow.playerTurn(cards, deck);
 
         //Then: Draw card is not called.
         verify(drawCard, times(0)).draw(deck);
@@ -68,25 +81,63 @@ public class PlayerFlowTest {
         System.setIn(new ByteArrayInputStream(userInput.getBytes())); //Filling in with fake user input
         List<String> deck = new ArrayList<String>();
         List<String> cards = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+
 
         //Given: Player has a 5,10.
         cards.add("5 of Hearts");
         cards.add("10 of Clubs");
+        values.add(5);
+        values.add(10);
 
         IDrawCard drawCard = mock(IDrawCard.class);
         when(drawCard.draw(deck)).thenReturn("Ace of Spades");
 
+        ICardValues cardValues = mock(ICardValues.class);
+        when(cardValues.getCardValues(cards)).thenReturn(values);
+
         IDisplayFlow displayFlow = mock(IDisplayFlow.class);
 
         //When: Player draws.
-        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow);
-        playerFlow.playerTurn(cards,deck);
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues);
+        playerFlow.playerTurn(cards, deck);
 
 
         //Then: Cards are displayed.
         verify(displayFlow, times(1)).displayCards("player", cards, false);
     }
 
+    @Test
+    public void getCardValuesOnce() {
+        String userInput = "D\nH";
+        System.setIn(new ByteArrayInputStream(userInput.getBytes())); //Filling in with fake user input
+        List<String> deck = new ArrayList<String>();
+        List<String> cards = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+
+        //Given: Player has a 5,10.
+        cards.add("5 of Hearts");
+        cards.add("10 of Clubs");
+        values.add(5);
+        values.add(10);
+
+        IDrawCard drawCard = mock(IDrawCard.class);
+        when(drawCard.draw(deck)).thenReturn("Ace of Spades");
+
+        ICardValues cardValues = mock(ICardValues.class);
+        when(cardValues.getCardValues(cards)).thenReturn(values);
+
+
+        IDisplayFlow displayFlow = mock(IDisplayFlow.class);
+
+
+        //When: Player draws.
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues);
+        playerFlow.playerTurn(cards, deck);
+
+        //Then: function card values is called once.
+        verify(cardValues, times(1)).getCardValues(cards);
+    }
 
 
 }
