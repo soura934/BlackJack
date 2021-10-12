527a5eda61d4597df12c1795e9fee2ef352003ac
ramshaessa <ressa@luc.edu>
2021-10-12 12:48:08 -0500

    new changes

--- src/main/java/com/saggezza/blackjack/PlayerFlow.java
+++ src/main/java/com/saggezza/blackjack/PlayerFlow.java
@@ -8,11 +8,13 @@ public class PlayerFlow implements IPlayerFlow {
     private IDrawCard drawCard;
     private IDisplayFlow displayFlow;
     private ICardValues cardValues;
+    private ICalculateScore calculateObject;
 
-    public PlayerFlow(IDrawCard drawCard, IDisplayFlow displayFlow, ICardValues cardValues) {
+    public PlayerFlow(IDrawCard drawCard, IDisplayFlow displayFlow, ICardValues cardValues, ICalculateScore calculateObject) {
         this.drawCard = drawCard;
         this.displayFlow = displayFlow;
         this.cardValues = cardValues;
+        this.calculateObject = calculateObject;
     }
 
     public void playerTurn(List<String> cards, List<String> deck) {
@@ -26,6 +28,7 @@ public class PlayerFlow implements IPlayerFlow {
             cards.add(newCard);
             displayFlow.displayCards("player", cards, false);
            List <Integer> cardVals  = cardValues.getCardValues(cards);
+           int score = calculateObject.calculate(cardVals);
 
             System.out.println("Enter H to hold or D to Draw");
             userInput = scan.nextLine();
--- src/test/java/com/saggezza/blackjack/PlayerFlowTest.java
+++ src/test/java/com/saggezza/blackjack/PlayerFlowTest.java
@@ -32,10 +32,13 @@ public class PlayerFlowTest {
         ICardValues cardValues = mock(ICardValues.class);
         when(cardValues.getCardValues(cards)).thenReturn(values);
 
+        ICalculateScore calculateScore = mock(ICalculateScore.class);
+        when(calculateScore.calculate(values)).thenReturn(16);
+
         IDisplayFlow displayFlow = mock(IDisplayFlow.class);
 
         //When: Player decides to draw.
-        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues);
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues, calculateScore);
         playerFlow.playerTurn(cards, deck);
 
         //Then: Draw card is called once.
@@ -64,10 +67,13 @@ public class PlayerFlowTest {
         ICardValues cardValues = mock(ICardValues.class);
         when(cardValues.getCardValues(cards)).thenReturn(values);
 
+        ICalculateScore calculateScore = mock(ICalculateScore.class);
+        when(calculateScore.calculate(values)).thenReturn(16);
+
         IDisplayFlow displayFlow = mock(IDisplayFlow.class);
 
         //When: Player stands.
-        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues);
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues, calculateScore);
         playerFlow.playerTurn(cards, deck);
 
         //Then: Draw card is not called.
@@ -96,10 +102,13 @@ public class PlayerFlowTest {
         ICardValues cardValues = mock(ICardValues.class);
         when(cardValues.getCardValues(cards)).thenReturn(values);
 
+        ICalculateScore calculateScore = mock(ICalculateScore.class);
+        when(calculateScore.calculate(values)).thenReturn(16);
+
         IDisplayFlow displayFlow = mock(IDisplayFlow.class);
 
         //When: Player draws.
-        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues);
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues, calculateScore);
         playerFlow.playerTurn(cards, deck);
 
 
@@ -127,16 +136,53 @@ public class PlayerFlowTest {
         ICardValues cardValues = mock(ICardValues.class);
         when(cardValues.getCardValues(cards)).thenReturn(values);
 
+        ICalculateScore calculateScore = mock(ICalculateScore.class);
+        when(calculateScore.calculate(values)).thenReturn(16);
+
 
         IDisplayFlow displayFlow = mock(IDisplayFlow.class);
 
 
         //When: Player draws.
-        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues);
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues, calculateScore);
         playerFlow.playerTurn(cards, deck);
 
         //Then: function card values is called once.
         verify(cardValues, times(1)).getCardValues(cards);
+    }
+
+    @Test
+    public void getHandScore() {
+        String userInput = "D\nH";
+        System.setIn(new ByteArrayInputStream(userInput.getBytes())); //Filling in with fake user input
+        List<String> deck = new ArrayList<String>();
+        List<String> cards = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+
+        //Given: Player has a 5,10.
+        cards.add("5 of Hearts");
+        cards.add("10 of Clubs");
+        values.add(5);
+        values.add(10);
+
+        IDrawCard drawCard = mock(IDrawCard.class);
+        when(drawCard.draw(deck)).thenReturn("Ace of Spades");
+
+        ICardValues cardValues = mock(ICardValues.class);
+        when(cardValues.getCardValues(cards)).thenReturn(values);
+
+        ICalculateScore calculateScore = mock(ICalculateScore.class);
+        when(calculateScore.calculate(values)).thenReturn(16);
+
+        IDisplayFlow displayFlow = mock(IDisplayFlow.class);
+
+
+        //When: Player draws.
+        IPlayerFlow playerFlow = new PlayerFlow(drawCard, displayFlow, cardValues, calculateScore);
+        playerFlow.playerTurn(cards, deck);
+
+        //Then: function card values is called once.
+        verify(calculateScore, times(1)).calculate(values);
     }
 
 
