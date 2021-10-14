05ec5d85aaaaeba8d1277b35d3d318753f7ceae2
chuckkainrath <chuckkainrath@gmail.com>
2021-10-14 09:36:19 -0500

    finished betinputflow

--- src/main/java/com/saggezza/blackjack/CalculateBet.java
+++ src/main/java/com/saggezza/blackjack/CalculateBet.java
@@ -3,22 +3,22 @@ package com.saggezza.blackjack;
 public class CalculateBet implements ICalculateBet {
     public double calculate(double money, int bet, String gameResult) {
         money = money - bet;
-        if (gameResult == "natural") {
+        if (gameResult.equals("natural")) {
             money += (bet * 2.5);
-            System.out.println("You bet: " + bet + "and your new amount is :" + money);
+            System.out.println("You bet: " + bet + " and your new amount is : " + money);
 
             return money;
-        } else if (gameResult == "win") {
+        } else if (gameResult.equals("win")) {
             money += (bet * 2);
-            System.out.println("You bet: " + bet + "and your new amount is :" + money);
+            System.out.println("You bet: " + bet + " and your new amount is : " + money);
 
             return money;
-        } else if (gameResult == "loose") {
-            System.out.println("You bet: " + bet + "and your new amount is :" + money);
+        } else if (gameResult.equals("loose")) {
+            System.out.println("You bet: " + bet + " and your new amount is : " + money);
 
             return money;
         } else {
-            System.out.println("You bet: " + bet + "and your new amount is :" + money);
+            System.out.println("You bet: " + bet + " and your new amount is : " + money);
         }
         return money + bet;
     }
