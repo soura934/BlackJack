52e86f76de9be38cd8ead4cf2532d306f9b71c07
chuckkainrath <chuckkainrath@gmail.com>
2021-10-15 10:05:48 -0500

    commit

--- src/main/java/com/saggezza/blackjack/PlayerCountValidation.java
+++ src/main/java/com/saggezza/blackjack/PlayerCountValidation.java
@@ -1,5 +1,8 @@
 package com.saggezza.blackjack;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PlayerCountValidation implements IPlayerCountValidation {
     public boolean validate(int num) {
         return num > 0 && num <= 5;
